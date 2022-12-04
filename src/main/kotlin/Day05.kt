f437ce0ac31e4e5066832c1b8ee3c3282f0661ca
Cliff Cotino <cliffcotino@gmail.com>
2022-12-04 11:33:53 +0100

    Day 5 Scaffolding

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -1,12 +1,17 @@
 
 class Day05 : Day() {
 
+    fun String.toDomain(): Any =
+        TODO()
+
     fun test1(file: String): Int =
         readLines(file)
+            .map { it.toDomain() }
             .count()
 
     fun test2(file: String): Int =
         readLines(file)
+            .map { it.toDomain() }
             .count()
 
 }
