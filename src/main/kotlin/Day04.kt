ea80821456f58940307f0e6557026c3d4e6b2913
Cliff Cotino <cliffcotino@gmail.com>
2023-12-04 21:05:30 +0100

    Day 4

---
+++ src/main/kotlin/Day04.kt
@@ -0,0 +1,60 @@
+import kotlin.math.pow
+
+fun main() {
+
+    fun String.toListOfInt(): List<Int> {
+        return trim().split(Regex("\\s+")).map { it.toInt() }
+    }
+
+    fun String.toCardGame(): CardGame {
+        return CardGame(
+            id = substringBetween("Card ", ":").trim().toInt(),
+            winningNumbers = substringAfter(":").substringBefore("|").toListOfInt(),
+            ownNumbers = substringAfter("|").toListOfInt()
+        )
+    }
+
+    fun part1(input: List<String>): Int {
+        return input
+            .map { s -> s.toCardGame() }
+            .sumOf { it.cardsCorrectPoints() }
+    }
+
+    fun part2(input: List<String>): Int {
+        val cardGames = input.map { s -> s.toCardGame() }
+        val runningTally = MutableList (cardGames.size) { 1 }
+        for (c in cardGames.indices) {
+            val cardGame = cardGames[c]
+            val cardsCorrect = cardGame.cardsCorrect()
+            repeat(runningTally[c]) {
+                repeat(cardsCorrect.size) { i ->
+                    runningTally[c + i + 1] = runningTally[c + i + 1] + 1
+                }
+            }
+        }
+        return runningTally.sum()
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+    assertEquals(part1(testInput), 13)
+    assertEquals(part2(testInput), 30)
+
+    val input = readInput("Day04")
+    part1(input).println()
+    part2(input).println()
+}
+
+private data class CardGame(val id: Int, val winningNumbers: List<Int>, val ownNumbers: List<Int>) {
+
+    fun cardsCorrect(): List<Int> = ownNumbers.filter { it in winningNumbers }
+
+    fun cardsCorrectPoints(): Int {
+        val correct = cardsCorrect()
+        return if (correct.isEmpty()) {
+            0
+        } else {
+            2.0.pow(correct.size.toDouble() - 1).toInt()
+        }
+    }
+}
