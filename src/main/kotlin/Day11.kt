e301c6e67dc666acc6950d7d94773f9368fe5296
Cliff Cotino <cliffcotino@gmail.com>
2023-12-16 23:53:12 +0100

    Day 11 refactoring

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -7,17 +7,27 @@ fun main() {
     fun part1(input: List<String>): Int {
         val emptyRows = input.findEmptyRows()
         val emptyColumns = input.findEmptyColumns()
-        val expandedGalaxy = input.expand(emptyRows = emptyRows, emptyColumns = emptyColumns)
-        val galaxyPositions = expandedGalaxy.findGalaxyPositions()
-        return galaxyPositions.sumOf { it.distanceToOtherGalaxies(galaxyPositions) } / 2
+        val galaxyPositions = input.findGalaxyPositions()
+        return distanceBetweenGalaxies(galaxyPositions, 2, emptyRows, emptyColumns)
     }
 
-    fun part2(input: List<String>, scale: Int): Int {
+    fun part2(input: List<String>, scaleFactor: Int): Int {
         val emptyRows = input.findEmptyRows()
         val emptyColumns = input.findEmptyColumns()
         val galaxyPositions = input.findGalaxyPositions()
-        return galaxyPositions.sumOf { it.distanceToOtherGalaxies(galaxyPositions, scale, emptyRows, emptyColumns) } / 2
+        return distanceBetweenGalaxies(galaxyPositions, scaleFactor, emptyRows, emptyColumns)
     }
+
+    part2("""
+        ..#..
+        .....
+        ..#..
+    """.trimIndent().lines(), 1000_000).println()
+
+    part2("""
+        .#.#.
+        .....
+    """.trimIndent().lines(), 2).println()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day11_test")
@@ -28,75 +38,55 @@ fun main() {
     val input = readInput("Day11")
     part1(input).println() // 9957702
     part2(input, 1_000_000).println() // ?
+    // wrong: 1139825014, 1140337246
 }
 
-private fun List<String>.expand(emptyRows: List<Int>, emptyColumns: List<Int>): List<String> {
-    val expandedGalaxy = toMutableList()
-    var rowsAdded = 0
-    emptyRows.forEach { i ->
-        val emptyRow = expandedGalaxy[emptyRows[0]]
-        expandedGalaxy.add(i + rowsAdded, emptyRow)
-        rowsAdded++
-    }
-    return expandedGalaxy.map {
-        val row = it.toMutableList()
-        var columnsAdded = 0
-        emptyColumns.forEach { i ->
-            row.add(i + columnsAdded, '.')
-            columnsAdded++
-        }
-        row.joinToString("")
-    }
-}
-
-private data class GalaxyPosition(val char: Char, val x: Int, val y: Int)
+private data class GalaxyPosition(val x: Int, val y: Int, val char: Char)
 
 private fun List<String>.findGalaxyPositions(): List<GalaxyPosition> {
-    return (indices).flatMap { r ->
-        this[r]
+    return (indices).flatMap { row ->
+        this[row]
             .toList()
-            .mapIndexed { index, char -> GalaxyPosition(char = char, x = index, y = r) }
+            .mapIndexed { index, char -> GalaxyPosition(char = char, x = index, y = row) }
             .filter { p -> p.char == '#' }
     }
 }
 
-private fun GalaxyPosition.distanceToOtherGalaxies(galaxyPositions: List<GalaxyPosition>): Int {
-    return galaxyPositions.sumOf { other ->
-        if (other == this) {
-            0
-        } else {
-            abs(x - other.x) + abs(y - other.y)
-        }
+private fun distanceBetweenGalaxies(
+    galaxyPositions: List<GalaxyPosition>,
+    scaleFactor: Int,
+    emptyRows: List<Int>,
+    emptyColumns: List<Int>
+): Int {
+    val pairs = findUniquePairs(galaxyPositions)
+    return pairs.sumOf { pair ->
+        val g1 = pair.first
+        val g2 = pair.second
+        val intersectionsX = emptyColumns.intersections(g1.x, g2.x)
+        val distanceX = abs(g1.x - g2.x) + (intersectionsX * (scaleFactor - 1))
+
+        val intersectionsY = emptyRows.intersections(g1.y, g2.y)
+        val distanceY = abs(g1.y - g2.y) + (intersectionsY * (scaleFactor - 1))
+
+        distanceX + distanceY
     }
 }
 
-private fun GalaxyPosition.distanceToOtherGalaxies(galaxyPositions: List<GalaxyPosition>,
-                                                   scale: Int, emptyRows: List<Int>, emptyColumns: List<Int>): Int {
-    return galaxyPositions.sumOf { other ->
-        if (other == this) {
-            0
-        } else {
-            val intersectionsX = emptyRows.intersections(x, other.x)
-            val distanceX = abs(x - other.x) + (intersectionsX * scale)
-
-            val intersectionsY = emptyColumns.intersections(y, other.y)
-            val distanceY = abs(y - other.y) + (intersectionsY * scale)
-
-            distanceX + distanceY
-        }
-    }
+private fun findUniquePairs(galaxyPositions: List<GalaxyPosition>): List<Pair<GalaxyPosition, GalaxyPosition>> {
+    return galaxyPositions
+        .flatMap { g1 -> galaxyPositions.map { g2 -> g1 to g2 } }
+        .filter { pair -> pair.first != pair.second }
+        .distinctBy { pair -> listOf(pair.first, pair.second).sortedBy { g -> "x=${g.x}, y=${g.y}" } }
 }
 
 private fun List<Int>.intersections(v1: Int, v2: Int): Int {
     val lower = min(v1, v2)
     val upper = max(v1, v2)
-    val intersections = count { v -> v >= lower && v <= upper }
-    return intersections
+    return count { v -> v in lower..upper }
 }
 
 private fun List<String>.findEmptyRows() =
-    mapIndexed { index, row -> Pair(index, row) }
-        .filterEmpty()
+    mapIndexed { index, row -> Pair(index, row) }.filterEmpty()
 
 private fun List<String>.findEmptyColumns() =
     (0 until this[0].length)
@@ -104,10 +94,9 @@ private fun List<String>.findEmptyColumns() =
         .filterEmpty()
 
 private fun List<Pair<Int, String>>.filterEmpty() =
-    filter { p -> isSpaceOnly(p.second) }
-        .map { p -> p.first }
+    filter { p -> isEmptySpace(p.second) }.map { p -> p.first }
 
-private fun isSpaceOnly(value: String) = value.count { it == '.' } == value.length
+private fun isEmptySpace(value: String) = value.count { it == '.' } == value.length
 
 private fun getColumn(rows: List<String>, column: Int) =
     List(rows.size) { rows[it][column] }.joinToString("")
